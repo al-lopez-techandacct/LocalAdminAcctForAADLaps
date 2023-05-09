a8910ad67c21244a3be320228e948a5e4f63561e
al-lopez-techandacct <al.lopez@techandacct.com>
2023-05-09 19:26:53 -0400

    Working on detection method

--- DetectionMethod.ps1
+++ DetectionMethod.ps1
@@ -1,4 +1,25 @@
 #$user = "$env:COMPUTERNAME\$env:USERNAME"
 $user = "$env:COMPUTERNAME\alopez"
 $group = 'Administrators'
-$isInGroup = (Get-LocalGroupMember $group).Name -contains $user
+
+
+
+try
+{   
+    $isInGroup = (Get-LocalGroupMember $group).Name -contains $user
+    if (($isInGroup){
+        #Below necessary for Intune as of 10/2019 will only remediate Exit Code 1
+        Write-Host "Match"
+        Return $results.count
+        exit 1
+    }
+    else{
+        Write-Host "No_Match"
+        exit 0
+    }    
+}
+catch{
+    $errMsg = $_.Exception.Message
+    Write-Error $errMsg
+    exit 1
+}
